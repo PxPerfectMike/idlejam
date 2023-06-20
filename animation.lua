ad43c579d5216a533d10a90f16e449ebcc4ac1e7
pxperfectmike <pxperfectmike@gmail.com>
2023-06-20 00:54:26 -0700

    implement sky and make top ui bar larger

--- animation.lua
+++ animation.lua
@@ -21,11 +21,23 @@ character_table = {
     {name = "spaceship", frames = {88, 90, 92, 94}}
 }
 
-ground_table = {
-    {name = "grass", frames = {120, 121, 122, 123, 124, 125, 126, 127}},
+ground_table = {{name = "grass", frames = {120, 121, 122, 123, 124, 125, 126, 127}}}
+
+sky_table = {
+    {name = "sky", row = 1, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
+    {name = "sky", row = 2, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
+    {name = "sky", row = 3, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
+    {name = "sky", row = 4, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
+    {name = "sky", row = 5, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
+    {name = "sky", row = 6, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
+    {name = "sky", row = 7, frames = {112, 113, 114, 115, 116, 117, 118, 119}},
+    {name = "sky", row = 8, frames = {128, 129, 130, 131, 132, 133, 134, 135}},
+    {name = "sky", row = 9, frames = {144, 145, 146, 147, 148, 149, 150, 151}},
+    {name = "sky", row = 10, frames = {160, 161, 162, 163, 164, 165, 166, 167}},
+    {name = "sky", row = 11, frames = {176, 177, 178, 179, 180, 181, 182, 183}},
+    {name = "sky", row = 12, frames = {192, 193, 194, 195, 196, 197, 198, 199}},
 }
 
-sky_table = {{name = "sky", frames = {}}}
 
 -- Add names as keys
 for i, v in ipairs(character_table) do
@@ -34,7 +46,7 @@ end
 
 function _init()
     cls()
-    
+    sky_speed = 0.3  -- initial sky speed
     palt(14, true) -- pink color as transparency is true
     palt(0, false) -- black color as transparency is false
 
@@ -43,11 +55,15 @@ function _init()
         add(ground, {x = i * 8, frame = ground_table[1].frames[flr(rnd(#ground_table[1].frames)) + 1]})
     end
 
-    -- initialize sky with enough sprites to fill the screen
-    for i = 0, 15 do
-        add(sky, {x = i * 8, frame = sky_table[1].frames[flr(rnd(#sky_table[1].frames)) + 1]})
+-- initialize sky with enough sprites to fill the screen
+for i = 0, 15 do
+        for j = 1, 12 do
+            -- for each sprite, select a random frame from the corresponding row in the sky_table
+            add(sky, {x = i * 8, y = sky_arr[j], frame = sky_table[j].frames[flr(rnd(#sky_table[j].frames)) + 1], row = j})
+        end
     end
 end
+
 
 function _update()
     character_switch()
@@ -55,20 +71,21 @@ function _update()
     animation(character_table[level].frames, speed)
 end
 ground_speed_control = 21
-
+sky_arr = {25, 33, 41, 49, 57, 65, 73, 81, 89, 97, 105, 113, 121, 129, 137, 145, 153, 161, 169, 177, 185, 193}
 function _draw()
     cls()
 
-    -- draw sky sprites
-    for i, s in pairs(sky) do
-        spr(s.frame, s.x, 0, 1, 15)
-        s.x -= speed
-        -- if a sprite goes off screen on the left, move it to the right side and change its frame
-        if s.x < -8 then
-            s.x = s.x + 128
-            s.frame = sky_table[1].frames[flr(rnd(#sky_table[1].frames)) + 1]
-        end
+-- draw sky sprites
+for i, s in pairs(sky) do
+    spr(s.frame, s.x, s.y, 1, 1)
+    s.x -= sky_speed  -- use sky_speed here instead of speed
+    -- if a sprite goes off screen on the left, move it to the right side and change its frame
+    if s.x < -8 then
+        s.x = s.x + 128
+        -- select a new sprite from the correct row
+        s.frame = sky_table[s.row].frames[flr(rnd(#sky_table[s.row].frames)) + 1]
     end
+end
 
     -- draw ground sprites
     for i, g in pairs(ground) do
@@ -81,15 +98,16 @@ function _draw()
         end
     end
 
-    rectfill(0, 0, 128, 16, 13) -- top bar
-    rectfill(96, 17, 128, 128, 6) -- right bar
-    rectfill(0, 17, 96, 119, 12) -- sky area
-    line(0, 16, 128, 16, 7) -- top separator
-    line(96, 17, 96, 128, 7) -- right separator
+    rectfill(0, 0, 128, 25, 13) -- top bar
+    rectfill(96, 25, 128, 128, 6) -- right bar
+    -- rectfill(0, 17, 96, 119, 12) -- sky area
+    line(0, 25, 128, 25, 7) -- top separator
+    line(96, 25, 96, 128, 7) -- right separator
 
     spr(current_frame, 42, 104, 2, 2)
-    print("level: "..level, 2, 2, 7)
-    print("character speed: "..speed, 2, 10, 7)
+    print("level: "..level, 2, 10, 7)
+    print("character speed: "..speed, 2, 18, 7)
+    print("streamer name", 2, 2, 7)
 end
 
 function animation(sprite, animation_speed)
@@ -125,4 +143,10 @@ function speed_switch()
     elseif speed < 1 then
         speed = 19
     end
+
+    -- Here's the updated mapping for sky_speed.
+    -- We raise 1.1 to the power of (speed - 1), and then divide by 20 to get the range we want.
+    sky_speed = 1.1^(speed - 1) / 20
 end
+
+
