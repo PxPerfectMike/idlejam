212164f1b8bf4919e0835ac29937588e3e054da8
pxperfectmike <pxperfectmike@gmail.com>
2023-06-20 02:46:08 -0700

    optimize code

--- animation.lua
+++ animation.lua
@@ -12,6 +12,13 @@ rightmost_ground_x = 128
 ground = {}
 sky = {}
 
+-- Function to generate frames
+function generate_frames(start, count)
+    local frames = {}
+    for i = 0, count do add(frames, start + i) end
+    return frames
+end
+
 -- all tables spawn by index and must be in the same order to maintain theme consistency
 -- table of sprite numbers for the run animation
 character_table = {
@@ -23,30 +30,23 @@ character_table = {
 }
 
 -- all tables spawn by index and must be in the same order to maintain theme consistency
-ground_table = {
-    {name = "grass", frames = {120, 121, 122, 123, 124, 125, 126, 127}},
-    {name = "ds_ground", frames = {152, 153, 154, 155, 156, 157, 158, 159}},
-    {name = "watermelon", frames = {168, 169, 170, 171, 172, 173, 174, 175}},
-    {name = "graveyard", frames = {136, 137, 138, 139, 140, 141, 142, 143}},
-    {name = "space", frames = {184, 185, 186, 187, 188, 189, 190, 191}}
-}
+ground_start_frames = {120, 152, 168, 136, 184}
+ground_names = {"grass", "ds_ground", "watermelon", "graveyard", "space"}
+
+ground_table = {}
+for i, start_frame in ipairs(ground_start_frames) do
+    ground_table[i] = {name = ground_names[i], frames = generate_frames(start_frame, 7)}
+end
 
 -- all tables spawn by index and must be in the same order to maintain theme consistency
-sky_table = {
-    {name = "sky", row = 1, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
-    {name = "sky", row = 2, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
-    {name = "sky", row = 3, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
-    {name = "sky", row = 4, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
-    {name = "sky", row = 5, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
-    {name = "sky", row = 6, frames = {96, 97, 98, 99, 100, 101, 102, 103}},
-    {name = "sky", row = 7, frames = {112, 113, 114, 115, 116, 117, 118, 119}},
-    {name = "sky", row = 8, frames = {128, 129, 130, 131, 132, 133, 134, 135}},
-    {name = "sky", row = 9, frames = {144, 145, 146, 147, 148, 149, 150, 151}},
-    {name = "sky", row = 10, frames = {160, 161, 162, 163, 164, 165, 166, 167}},
-    {name = "sky", row = 11, frames = {176, 177, 178, 179, 180, 181, 182, 183}},
-    {name = "sky", row = 12, frames = {192, 193, 194, 195, 196, 197, 198, 199}},
-}
-
+sky_table = {}
+for i = 1, 12 do
+    local start_frame = 96
+    if i > 6 then
+        start_frame = start_frame + ((i - 6) * 16)
+    end
+    sky_table[i] = {name = "sky", row = i, frames = generate_frames(start_frame, 7)}
+end
 
 -- Add names as keys
 for i, v in ipairs(character_table) do
@@ -115,44 +115,32 @@ end
     line(96, 25, 96, 128, 7) -- right separator
 
     spr(current_frame, 42, 104, 2, 2)
+    print("streamer name", 2, 2, 8)
     print("level: "..level, 2, 10, 7)
     print("character speed: "..speed, 2, 18, 7)
-    print("streamer name", 2, 2, 7)
 end
 
 function animation(sprite, animation_speed)
     local actual_speed = ground_speed_control - animation_speed
-    if frame % actual_speed == 0 then
-        current_frame = sprite[(frame / actual_speed % #sprite) + 1]
-    end
+
+    if (frame % actual_speed == 0) current_frame = sprite[(frame / actual_speed % #sprite) + 1]
+
     frame += 1
-    return actual_speed
+
 end
 
 function character_switch()
-    if btnp(➡️) then
-        level += 1
-    elseif btnp(⬅️) then
-        level -= 1
-    end
-    if level > 5 then
-        level = 1
-    elseif level < 1 then
-        level = 5
-    end
+    if (btnp(➡️)) level += 1
+    if (btnp(⬅️)) level -= 1
+    if (level > 5) level = 1
+    if (level < 1) level = 5
 end
 
 function speed_switch()
-    if btnp(⬆️) then
-        speed += 1
-    elseif btnp(⬇️) then
-        speed -= 1
-    end
-    if speed > 19 then
-        speed = 1
-    elseif speed < 1 then
-        speed = 19
-    end
+    if (btnp(⬆️)) speed += 1
+    if (btnp(⬇️)) speed -= 1
+    if (speed > 19) speed = 1
+    if (speed < 1) speed = 19
 
     -- Here's the updated mapping for sky_speed.
     -- We raise 1.1 to the power of (speed - 1), and then divide by 20 to get the range we want.
