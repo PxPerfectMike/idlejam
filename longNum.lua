cbe41a45efc715b920c92ed72c95945c5a41bc06
Austin Z <137doctorwho2015@gmail.com>
2023-06-20 23:11:17 -0700

    moved things out of

--- longNum.lua
+++ longNum.lua
@@ -353,3 +353,69 @@ function round_up_long_num(num, value_place)
     end
     return num
 end
+
+--=======================================================================
+-- math to find cost and production
+--=======================================================================
+
+--[[
+    each generator needs
+    - initial cost
+    - cost coefficient (aka cost growth)
+    - inital Time/rate
+    - base production (rate)
+]]
+
+-- credit for the equations goes to Anthony Percorella, "The Math of Idle Games, Part 1", Kongregate Developers Blog
+--[[
+    find how much the next upgrade cost
+    b = the base price
+    r = the price growth rate exponent
+]]
+function cost_next(b, r)
+    return b * r
+end
+
+--[[
+      find how much the next upgrade cost
+      b = the base price
+      r = the price growth rate exponent
+      k = the number of generators currently owned
+  ]]
+function cost_next(b, r, k)
+    return b * pow(r, k)
+end
+
+--[[
+      takes a float and rounds it to format it like money
+  ]]
+function float_to_money(x)
+    --x * 1000 % 10
+end
+
+-- find how much each generator makes per unit of measurement
+function total_production(_production_base, _owned, _multipliers)
+    return _production_base * _owned * _multipliers
+end
+
+--[[
+      find how much it costs to buy in bulk ammount
+      n = the number of generators to buy
+      b = the base price
+      r = the price growth rate exponent
+      k = the number of generators currently owned
+  ]]
+function bulk_buy_cost(b, r, k, n)
+    return b * pow(r, k) * (pow(r, n) - 1) / (r - 1)
+end
+
+--[[
+      what is the maximum ammount of units you can buy
+      b = the base price
+      r = the price growth rate exponent
+      k = the number of generators currently owned
+      c = the amount of currency owned
+  ]]
+function max_buy_ammount(b, r, k, c)
+    return flr(ln(c * (r - 1) / b * pow(r, k) + 1) / ln(r))
+end
